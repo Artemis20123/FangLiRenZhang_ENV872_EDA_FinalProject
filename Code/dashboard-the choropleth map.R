dcf63c7a83c367efdb14c046ac397a9975861856
Artemis20123 <jl1063@duke.edu>
2023-05-03 16:11:24 +0800

    ui update: add dropdown menu examples and tabitem information

--- Code/dashboard-the choropleth map.R
+++ Code/dashboard-the choropleth map.R
@@ -15,7 +15,55 @@ pm25_sf <- merge(city_boundaries,pm25_data, by = "city_id")
 
 # create UI
 ui <- dashboardPage(
-  dashboardHeader(title = "PM2.5 Distribution in Chinese Cities"),
+  dashboardHeader(
+    title = "PM2.5 Distribution in Chinese Cities",
+    dropdownMenu(type = "messages", badgeStatus = "success",
+                 messageItem("Support Team",
+                             "This is the content of a message.",
+                             time = "5 mins"
+                 ),
+                 messageItem("Support Team",
+                             "This is the content of another message.",
+                             time = "2 hours"
+                 ),
+                 messageItem("New User",
+                             "Can I get some help?",
+                             time = "Today"
+                 )
+    ),
+    
+    # Dropdown menu for notifications
+    dropdownMenu(type = "notifications", badgeStatus = "warning",
+                 notificationItem(icon = icon("users"), status = "info",
+                                  "5 new members joined today"
+                 ),
+                 notificationItem(icon = icon("warning"), status = "danger",
+                                  "Resource usage near limit."
+                 ),
+                 notificationItem(icon = icon("shopping-cart", lib = "glyphicon"),
+                                  status = "success", "25 sales made"
+                 ),
+                 notificationItem(icon = icon("user", lib = "glyphicon"),
+                                  status = "danger", "You changed your username"
+                 )
+    ),
+    
+    # Dropdown menu for tasks, with progress bar
+    dropdownMenu(type = "tasks", badgeStatus = "danger",
+                 taskItem(value = 20, color = "aqua",
+                          "Refactor code"
+                 ),
+                 taskItem(value = 40, color = "green",
+                          "Design new layout"
+                 ),
+                 taskItem(value = 60, color = "yellow",
+                          "Another task"
+                 ),
+                 taskItem(value = 80, color = "red",
+                          "Write documentation"
+                 )
+    )
+  ),
   dashboardSidebar(
     selectInput(inputId = "year", 
                 label = "Year",
@@ -34,6 +82,15 @@ ui <- dashboardPage(
         solidHeader = TRUE,
         leafletOutput("map")
       )
+    ),
+    tabItem(
+      tabName = "documentation",
+      h2("Dashboard Documentation"),
+      p("This dashboard displays the distribution of PM2.5 in Chinese cities from 2000 to 2021."),
+      p("To use the dashboard, select a year and month using the dropdown menus in the sidebar. The map will display the mean PM2.5 concentration for each city in the selected year and month."),
+      p("For more information, please see the following resources:"),
+      p(a("GitHub repository", href = "https://github.com/yourusername/yourrepository")),
+      p(a("Knit PDF of wrangling code", href = "https://yourdomain.com/yourfile.pdf"))
     )
   )
 )
